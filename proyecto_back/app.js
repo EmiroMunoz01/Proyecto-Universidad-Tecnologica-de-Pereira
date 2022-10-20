eba9304f373939421e87b98e5bef94a92745a2a0
EmiroMunoz01 <emiro_2001@icloud.com>
2022-10-20 09:45:40 -0500

    Creacion del modelo y el controlador, se hicieron pruebas en postman

--- proyecto_back/app.js
+++ proyecto_back/app.js
@@ -7,12 +7,11 @@ var database = require("./config/database");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var empleadosRouter = require('./routes/empleados.router');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,9 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //mongo connect
 database.mongoConnect();
-
+//Routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/empleados', empleadosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

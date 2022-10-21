8298c85cedbe82f5f27a1e08edde0be911a76fbe
EmiroMunoz01 <emiro_2001@icloud.com>
2022-10-21 11:25:11 -0500

    Se agrego el metodo get, para leer los datos desde el postman

--- proyecto_back/controllers/empleados.controller.js
+++ proyecto_back/controllers/empleados.controller.js
@@ -1,30 +1,35 @@
 const Empleado = require("../models/empleados.model");
 let response = {
-    msg: "",
-    exito: false,
+  msg: "",
+  exito: false,
 };
 
 exports.create = function (req, res) {
   let empleado = new Empleado({
-        nombre: req.body.nombre,
-        apellido_p: req.body.apellido_p,
-        apellido_m: req.body.apellido_m,
-        telefono: req.body.telefono,
-        email: req.body.email,
-        direccion: req.body.direccion,
+    nombre: req.body.nombre,
+    apellido_p: req.body.apellido_p,
+    apellido_m: req.body.apellido_m,
+    telefono: req.body.telefono,
+    email: req.body.email,
+    direccion: req.body.direccion,
   });
 
   empleado.save(function (err) {
     if (err) {
-            console.error(err),
-            response.exit = false,
-            response.msg = "Error al guardar el empledo";
-            res.json(response);
-            return;
-        }
+      console.error(err),
+        (response.exit = false),
+        (response.msg = "Error al guardar el empledo");
+      res.json(response);
+      return;
+    }
 
-        response.exito = true,
-        response.msg = "El empleado se guardo correctamente"
-        res.json(response)
+    (response.exito = true),
+      (response.msg = "El empleado se guardo correctamente");
+    res.json(response);
+  });
+};
+exports.find = function (req, res) {
+  Empleado.find(function (err, empleados) {
+    res.json(empleados);
   });
 };
--- proyecto_back/routes/empleados.router.js
+++ proyecto_back/routes/empleados.router.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const empleadosController = require("../controllers/empleados.controller");
 
 router.post("/", empleadosController.create);
+router.get("/", empleadosController.find);
 
 module.exports = router;

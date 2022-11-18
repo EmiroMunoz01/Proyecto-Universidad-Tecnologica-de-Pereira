ef4f002e992240e6ac40307a2d97022fa6063938
ydelgadom1020 <ydelgadom1020@gmail.com>
2022-11-18 14:22:16 -0500

    cambios4

--- empleados_front/src/components/login/login.js
+++ empleados_front/src/components/login/login.js
@@ -1,15 +1,15 @@
 import React from "react";
-import axios from "axios" ;
+import axios from "axios";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
-import "./login.css";
+import app from "../../app.json";
+import './login.css'
 import { isNull } from "util";
 import Cookies from "universal-cookie";
-import {calculaExtraccionSesion} from "../helper/helper";
-import app from "../app.json";
+import { calcularExpirarSesion } from "../helper/helper";
 import Loading from "../loading/loading";
 
+const {APIHOST} = app;
 const cookies = new Cookies();
-const { APIHOST } = app;
 
 export default class login extends React.Component {
   constructor(props) {
@@ -20,76 +20,72 @@ export default class login extends React.Component {
       pass: "",
     };
   }
-  iniciarSesion() {
-    this.setState({ loading: true });
+  iniciarSesion(){
+    this.setState({ loading: true});
     axios.post(`${APIHOST}/usuarios/login`, {
-      usuario: this.state.usuario, 
+      usuario: this.state.usuario,
       pass: this.state.pass,
-    }
-    )
-    .then((response) =>{
-      if(isNull(response.data.token)){
-        alert("usuario y/o contraseña invalidas");
-      }else{
-        cookies.set("_s", response.data.token,{
-          path:"/",
-          expires: calculaExtraccionSesion(),
-        });
+    })
+    .then((response) => {
+      if (isNull(response.data.token)){
+        alert("Usuario y/o contraseñas invalidos");
       }
-      this.setState({ loading: false });
-    })
-    .catch((err) =>{
+      else{
+        cookies.set('_s', response.data.token,
+        {
+          path: '/',
+          expires:calcularExpirarSesion(),
+        });
+        this.props.history.push(window.open('/empleados')
+        );
+      }
+      this.setState ({ loading: false});
+    } ) 
+    .catch((err) => {
       console.log(err);
-      this.setState({ loading: false });
-
-    })
+      this.setState ({ loading: false});
+    })  
   }
   render() {
     return (
-      <Container id="login-container">
+      <Container id="login-container" >
         <Loading show={this.state.loading} />
         <Row>
-          <Col>
+          <Col
+          sm="12"
+          xs="12"
+          md={{ span: 4, offset:4}}
+          lg={{ span: 4, offset:4}}
+          xl={{ span: 4, offset:4}}>
             <Row>
-              <h2>Iniciar Sesión</h2>
+              <h2>Iniciar sesión</h2>
             </Row>
-
             <Row>
-              <Col
-                sm="12"
-                xs="12"
-                md={{ span: 4, offset: 4 }}
-                lg={{ span: 4, offset: 4 }}
-                xl={{ span: 4, offset: 4 }}
-              >
-                <Form>
-                  <Form.Group>
-                    <Form.Label>Usuario</Form.Label>
-                    <Form.Control
-                      onChange={(e) =>
-                        this.setState({ usuario: e.target.value })
-                      }
-                    />
-                  </Form.Group>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label >Usuario</Form.Label>
+                  <Form.Control
+                    onChange={(e) => this.setState({ usuario: e.target.value })}
+                  />
+                </Form.Group>
 
-                  <Form.Group>
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control
-                      type="password"
-                      onChange={(e) => this.setState({ pass: e.target.value })}
-                    />
-                  </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label >Contraseña</Form.Label>
+                  <Form.Control
+                    type="password"
+                    onChange={(e) => this.setState({ pass: e.target.value })}
+                  />
+                </Form.Group>
 
-                  <Button
-                    variant="primary"
-                    onClick={() => {
-                      this.iniciarSesion();
-                    }}
-                  >
-                    Iniciar Sesión
-                  </Button>
-                </Form>
-              </Col>
+                <Button
+                  variant="primary"
+                  onClick={()=>{
+                    this.iniciarSesion();
+                  }}
+                >
+                  Iniciar sesión
+                </Button>
+              </Form>
             </Row>
           </Col>
         </Row>

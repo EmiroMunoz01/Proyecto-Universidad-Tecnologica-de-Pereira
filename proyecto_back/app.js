55fdd45269a3deddc0d6d09f8309d1383db70153
EmiroMunoz01 <emiro_2001@icloud.com>
2022-11-23 15:45:45 -0500

    Back

--- proyecto_back/app.js
+++ proyecto_back/app.js
@@ -5,7 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var database = require("./config/database");
 var auth = require("./auth/main_auth");
-var cors = require("cors")
 
 var empleadosRouter = require("./routes/empleados.router");
 var cacaosRouter = require("./routes/cacaos.router");
@@ -18,7 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
 //mongo connect
 database.mongoConnect();
